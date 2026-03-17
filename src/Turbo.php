c9c4ee93cc44e2a12f0d321d2811d3b8cca29825
emaia <131877+emaia@users.noreply.github.com>
2026-03-17 23:37:39 +0000

    Fix styling

--- src/Turbo.php
+++ src/Turbo.php
@@ -6,7 +6,7 @@ use Emaia\LaravelHotwireTurbo\Response as TurboResponse;
 
 class Turbo
 {
-    public static function response($content, $status = 200, array $headers = []): Response
+    public static function response($content, $status = 200, array $headers = []): TurboResponse
     {
         return new TurboResponse($content, $status, $headers);
     }
